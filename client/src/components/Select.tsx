9c118590a301fcb4b8b34205e03eb0141f1b8eb0
Sumit Duggal <sumit@coinjar.com>
2020-10-05 00:07:54 +1100

    added more props tp select component

--- client/src/components/Select.tsx
+++ client/src/components/Select.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-export const Select: React.FC = ({ children }) => (
-  <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+type SelectProps = {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+export const Select: React.FC<SelectProps> = ({
+  value,
+  onChange,
+  children,
+}) => (
+  <select
+    value={value}
+    onChange={onChange}
+    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+  >
     {children}
   </select>
 );
