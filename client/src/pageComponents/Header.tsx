72821703481d6a7b72cba5862ac98abefcd40c8a
Sumit Duggal <sumit@coinjar.com>
2020-10-04 19:11:53 +1100

    added PriceSort component

--- client/src/pageComponents/Header.tsx
+++ client/src/pageComponents/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Branding } from "../components/Branding";
+import { PriceSortSelect } from "./PriceSortSelect";
 
 export const Header = () => {
   return (
@@ -7,6 +8,7 @@ export const Header = () => {
       <div className="w-full md:w-auto border-b md:border-none p-5 sm:px-0 md:py-10 flex justify-center items-center">
         <Branding />
       </div>
+      <PriceSortSelect />
     </header>
   );
 };
