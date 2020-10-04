45e7e4e02dcc314360dfd50fcc63644e14832abc
Sumit Duggal <sumit@coinjar.com>
2020-10-04 17:34:18 +1100

    added Loading container

---
+++ client/src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./index.css";
+
+export const Loading = () => (
+  <div className="lds-ripple">
+    <div></div>
+    <div></div>
+  </div>
+);
---
+++ client/src/pageComponents/LoadingContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Loading } from "../components/Loading";
+
+export const LoadingContainer = () => (
+  <div className="w-full h-full flex justify-center items-center">
+    <Loading />
+  </div>
+);
