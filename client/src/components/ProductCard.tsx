30e66c51d1cdcbb268d03a5cb604d5e5db8aeac6
Sumit Duggal <sumit@coinjar.com>
2020-10-04 18:40:55 +1100

    added product card component

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { ProductItem } from "../AppProps";
+import { formatProductsPrice } from "../formatters";
 
 export const ProductCard = ({
   id,
@@ -10,5 +11,33 @@ export const ProductCard = ({
   imageUrl,
   quantityAvailable,
 }: ProductItem) => {
-  return <div>{name}</div>;
+  const isRetailPriceAvailable = retailPrice > 0;
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg flex flex-col">
+      <div className="relative">
+        <img className="w-full" src={imageUrl} alt={name} />
+        {quantityAvailable === 0 && (
+          <span
+            className="absolute uppercase right-0 bg-gray-600 px-3 py-1 text-sm text-white font-semibold mr-2 mb-2"
+            style={{ bottom: "1rem" }}
+          >
+            sold out
+          </span>
+        )}
+      </div>
+      <div className="px-6 py-4 flex flex-col justify-between flex-1">
+        <div className="font-bold text-xl text-center mb-2 capitalise">
+          {name}
+        </div>
+        <div className="flex flex-col items-center space-y-5">
+          <div className={`${isRetailPriceAvailable ? "line-through" : ""}`}>
+            {isRetailPriceAvailable && `$${formatProductsPrice(retailPrice)}`}
+          </div>
+          <div className="text-4xl font-bold">
+            ${formatProductsPrice(salePrice)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
