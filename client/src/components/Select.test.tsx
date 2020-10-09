f79cdace5d31dc7487ba9c12110cc8e8f722fe02
Sumit Duggal <sumit@coinjar.com>
2020-10-09 21:55:43 +1100

    fixed test name

--- client/src/components/Select.test.tsx
+++ client/src/components/Select.test.tsx
@@ -4,7 +4,7 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import { Select, SelectOption } from "./Select";
 
 const PRICE_SORT_OPTIONS = ['Highest', 'Lowest'];
-const selectElement = ({ id = "price-sort", label, defaultValue}: { id?: string, label?: string | undefined, defaultValue: string}) => <Select id={id} label={label} value={defaultValue} onChange={() => {}}>
+const selectElement = ({ id = "price-sort", label, passedValue}: { id?: string, label?: string | undefined, passedValue: string}) => <Select id={id} label={label} value={passedValue} onChange={() => {}}>
 {PRICE_SORT_OPTIONS.map((value) => (
         <SelectOption key={value} value={value}>
           {value}
@@ -12,8 +12,8 @@ const selectElement = ({ id = "price-sort", label, defaultValue}: { id?: string,
       ))}
 </Select>
 
-test("renders learn react link", () => {
- const { rerender } = render(selectElement({ label: "Sort Items by", defaultValue: PRICE_SORT_OPTIONS[0]}));
+test("renders Select component and values updates", () => {
+ const { rerender } = render(selectElement({ label: "Sort Items by", passedValue: PRICE_SORT_OPTIONS[0]}));
 
     // label is present if passed down to Select component
     expect(screen.getByLabelText("Sort Items by")).toBeInTheDocument()
@@ -23,10 +23,10 @@ test("renders learn react link", () => {
     fireEvent.change(screen.getByLabelText("Sort Items by"), {
       target: { value: PRICE_SORT_OPTIONS[1] },
     });
-    rerender(selectElement({ label: "Sort Items by", defaultValue: PRICE_SORT_OPTIONS[1]}));
+    rerender(selectElement({ label: "Sort Items by", passedValue: PRICE_SORT_OPTIONS[1]}));
     expect(screen.getByLabelText("Sort Items by").value).toBe(PRICE_SORT_OPTIONS[1])
   
     // label is absent if not passed down to Select component
-    rerender(selectElement({ defaultValue: PRICE_SORT_OPTIONS[0]}))
+    rerender(selectElement({ passedValue: PRICE_SORT_OPTIONS[0]}))
     expect(screen.queryByLabelText("Sort Items by")).not.toBeInTheDocument()
 });
